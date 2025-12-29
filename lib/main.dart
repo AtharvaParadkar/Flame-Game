e4962cfbfa3623310df5909b87c42e966f300201
AtharvaParadkar <atharvaparadkar91@gmail.com>
2025-12-29 22:50:47 +0530

    Forcing portrait resolution

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
-import 'package:flame/game.dart';
-import 'package:flame_game/flame_game.dart';
+import 'package:flame/flame.dart';
 import 'package:flame_game/game_app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  Flame.device.setPortrait();
+  // await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(GameApp());
 }
