7b16a8e1864728d2688445c42b3389fd5bde5431
AtharvaParadkar <atharvaparadkar91@gmail.com>
2025-12-29 21:59:26 +0530

    Add circle component and position it.

--- lib/flame_game.dart
+++ lib/flame_game.dart
@@ -1,9 +1,26 @@
+import 'dart:async';
+
 import 'package:flame/game.dart';
+import 'package:flame_game/player.dart';
 import 'package:flutter/material.dart';
 
 class FlameGameExample extends FlameGame {
   @override
   Color backgroundColor() {
-    return Colors.lightBlueAccent;
+    return Color.fromARGB(255, 0, 0, 30);
+  }
+
+  @override
+  FutureOr<void> onLoad() {
+    super.onLoad();
+
+    /// The world is essentially the whole game
+    // vector x,y 0,0 = center
+    world.add(
+      Player(position: Vector2(0, 0), radius: 50.0, color: Colors.green),
+    );
+    world.add(
+      Player(position: Vector2(300, 0), radius: 30.0, color: Colors.greenAccent),
+    );
   }
 }
